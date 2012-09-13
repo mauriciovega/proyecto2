def945b70a2cb5bd9fb59f5bd2ddcdb009433100
mauricio <mauriciovega81@hotmail.com>
2012-09-12 23:53:55 -0500

    hola mundo

--- proyectome/src/controlremoto.java
+++ proyectome/src/controlremoto.java
@@ -1,6 +1,18 @@
+
+
+
+
+
+
+
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
-
+import com.sun.lwuit.Display;
+import com.sun.lwuit.Form;
+import com.sun.lwuit.Label;
+import com.sun.lwuit.layouts.BorderLayout;
+import com.sun.lwuit.plaf.UIManager;
+import com.sun.lwuit.util.Resources;
 
 public class controlremoto extends MIDlet {
 
@@ -19,8 +31,26 @@ public class controlremoto extends MIDlet {
 	}
 
 	protected void startApp() throws MIDletStateChangeException {
+		
 		// TODO Auto-generated method stub
+		Display.init(this);
+		
+		try{
+			Resources r = Resources.open("/tema1.res");
+			UIManager.getInstance().setThemeProps(
+			r.getTheme(r.getThemeResourceNames()[0])
+			);
+			} catch (java.io.IOException e) {
+			}
+			Form f = new Form();
+			f.setTitle("Hello World");
+			f.setLayout(new BorderLayout());
+			f.addComponent(BorderLayout.CENTER, new Label("I am a Label"));
+			f.show();
+
+
+		}
 
 	}
 
-}
+
